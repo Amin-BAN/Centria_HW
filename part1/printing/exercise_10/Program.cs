ac786c245ebca7fb994f2588a56f4f398a215df0
Amin-BAN <md.amin@centria.fi>
2020-01-21 15:57:36 +0200

    exercise_10 done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -6,7 +6,11 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("I will tell a story, but I need some information." + "\n" + "Give a name for main character:");
+            String input_First = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            String input_Second = Console.ReadLine();
+           Console.WriteLine("Here is the story:" + "\n" + "Once upon a time there was a Data Scientist called Ada" + "\n" + "On her way to work, Ada often pondered what being Data Scientist meant to them." + "\n" + "When you work as a Data Scientist you meet interesting people." + "\n" + "Ada enjoys their work as Data Scientist, The end.");
 
         }
     }
