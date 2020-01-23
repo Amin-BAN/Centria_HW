d9232062423712bdb229c399941541523fafec53
Amin-BAN <md.amin@centria.fi>
2020-01-23 10:24:15 +0200

    exercise_15 done

--- part1/variables/exercise_15/Program.cs
+++ part1/variables/exercise_15/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give a string:");
+      string message = Console.ReadLine();
+
+      Console.WriteLine("Give an integer:");
+      string integar = Console.ReadLine();
+      int integarValue = Convert.ToInt32(integar);
+
+      Console.WriteLine("Give a double:");
+      string userInput = Console.ReadLine();
+      double doubleValue = Convert.ToDouble(userInput);
+
+      Console.WriteLine("Give a boolean:");
+      string input = Console.ReadLine();
+      bool booleanValue = System.Convert.ToBoolean(input);
+
+      Console.WriteLine("Your string: " + message);
+      Console.WriteLine("Your integer: " + integar);
+      Console.WriteLine("Your double: " + userInput);
+      Console.WriteLine("Your boolean: " + input);
+
 
     }
   }
