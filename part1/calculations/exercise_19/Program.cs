384ff77193076d221b47650f08ea4bc6fa084f30
Amin-BAN <md.amin@centria.fi>
2020-01-24 01:46:38 +0200

    exercise_19 done

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_19
   {
     public static void Main(string[] args)
     {
+       Console.WriteLine("Give the first number!");
+       string firstValue = Console.ReadLine();
+       int firstInput = Convert.ToInt32(firstValue);
 
+       Console.WriteLine("Give the second number!");
+       string secondValue = Console.ReadLine();
+       int secondInput = Convert.ToInt32(secondValue);
+
+       int sum = firstInput + secondInput;
+       Console.WriteLine(firstInput + " + " + secondInput + " = " + sum);
+
+      
       // Write your code here:
 
     }
