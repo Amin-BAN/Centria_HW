b784cd404f751f68ff12e3d81e1ab546f612ef4b
Amin-BAN <md.amin@centria.fi>
2020-01-23 23:27:06 +0200

    exercise_16 done

--- part1/calculations/exercise_16/Program.cs
+++ part1/calculations/exercise_16/Program.cs
@@ -6,6 +6,11 @@ namespace exercise_16
   {
     public static void Main(string[] args)
     {
+      int second = 60 * 60 * 24;
+      Console.WriteLine("How many days?");
+      string day_count = Console.ReadLine();
+      int input_Value = Convert.ToInt32(day_count);
+      Console.WriteLine(second * input_Value);
 
       // Write your code here:
 
