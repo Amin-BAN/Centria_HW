5f1f470c937b736c1e6ba28f0969bdcc92774223
Amin-BAN <md.amin@centria.fi>
2020-01-24 23:47:11 +0200

    exercise_25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -6,6 +6,19 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
+      int number = 1984;
+
+      Console.WriteLine("Give a number:");
+
+      string input = Console.ReadLine();
+
+      int value = Convert.ToInt32(input);
+
+      if (value == number)
+      {
+        Console.WriteLine("Orwell");
+      }
+
       // Write your code here:
 
     }
