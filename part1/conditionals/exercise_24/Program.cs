456c79b4a9d95a00306ec56a9665a698c50abc62
Amin-BAN <md.amin@centria.fi>
2020-01-24 15:07:16 +0200

    exercise_24 done

--- part1/conditionals/exercise_24/Program.cs
+++ part1/conditionals/exercise_24/Program.cs
@@ -6,6 +6,21 @@ namespace exercise_24
   {
     public static void Main(string[] args)
     {
+      int number = 120;
+
+      Console.WriteLine("Your speed:");
+
+      string value = Console.ReadLine();
+
+      int inputValue = Convert.ToInt32(value);
+
+      if (inputValue > number)
+
+      {
+        Console.WriteLine("Speeding!");
+      }
+
+
 
       // Write your code here:
       
