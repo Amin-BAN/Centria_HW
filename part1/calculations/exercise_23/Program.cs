4bfa8cf95700b0cfc51dd9391a548a8c6b1c05c4
Amin-BAN <md.amin@centria.fi>
2020-01-24 03:24:33 +0200

    exercise_23 done

--- part1/calculations/exercise_23/Program.cs
+++ part1/calculations/exercise_23/Program.cs
@@ -6,6 +6,23 @@ namespace exercise_23
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string firstValue = Console.ReadLine();
+      double firstInput = Convert.ToDouble(firstValue);
+
+      Console.WriteLine("Give the second number!");
+      string secondValue = Console.ReadLine();
+      double secondInput = Convert.ToDouble(secondValue);
+
+      double sum = firstInput + secondInput;
+      double subtraction = firstInput - secondInput;
+      double multiply = firstInput * secondInput;
+      double average = firstInput / secondInput;
+
+      Console.WriteLine(firstInput + " + " + secondInput + " = " + sum);
+      Console.WriteLine(firstInput + " - " + secondInput + " = " + subtraction);
+      Console.WriteLine(firstInput + " * " + secondInput + " = " + multiply);
+      Console.WriteLine(firstInput + " / " + secondInput + " = " + average);
 
       // Write your code here:
 
