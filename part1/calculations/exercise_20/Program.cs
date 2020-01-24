704b650b10be5533d3157ee4e212e03bbaae8b28
Amin-BAN <md.amin@centria.fi>
2020-01-24 02:03:56 +0200

    exercise_20 done

--- part1/calculations/exercise_20/Program.cs
+++ part1/calculations/exercise_20/Program.cs
@@ -6,6 +6,16 @@ namespace exercise_20
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string firstValue = Console.ReadLine();
+      int firstInput = Convert.ToInt32(firstValue);
+
+      Console.WriteLine("Give the second number!");
+      string secondValue = Console.ReadLine();
+      int secondInput = Convert.ToInt32(secondValue);
+
+      int multiply = firstInput * secondInput;
+      Console.WriteLine(firstInput + " * " + secondInput + " = " + multiply);
 
       // Write your code here:
 
