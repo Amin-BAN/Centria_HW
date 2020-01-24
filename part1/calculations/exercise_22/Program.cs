c04d0d90799729f7432d37839441d585765e2132
Amin-BAN <md.amin@centria.fi>
2020-01-24 02:49:32 +0200

    exercise_22 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -6,6 +6,21 @@ namespace exercise_22
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string firstValue = Console.ReadLine();
+      int firstInput = Convert.ToInt32(firstValue);
+
+      Console.WriteLine("Give the second number!");
+      string secondValue = Console.ReadLine();
+      int secondInput = Convert.ToInt32(secondValue);
+
+      Console.WriteLine("Give the third number!");
+      string thirdValue = Console.ReadLine();
+      int thirdInput = Convert.ToInt32(thirdValue);
+
+      double average = (firstInput + secondInput + thirdInput) / 3.0;
+      Console.WriteLine("The average is " + average);
+
 
       // Write your code here:
 
