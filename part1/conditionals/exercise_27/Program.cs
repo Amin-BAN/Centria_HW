d2dc259090d8885792795ae5f0c7012ed5811849
Amin-BAN <md.amin@centria.fi>
2020-01-25 00:30:50 +0200

    exercise_27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -6,6 +6,24 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
+      int number = 0;
+
+      Console.WriteLine("Give a number:");
+
+      string input = Console.ReadLine();
+
+      int value = Convert.ToInt32(input);
+
+      if (number < value)
+      {
+        Console.WriteLine("It is positive");
+      }
+      else
+      {
+        Console.WriteLine("It is not positive");
+      }
+
+
       // Write your code here:
 
     }
