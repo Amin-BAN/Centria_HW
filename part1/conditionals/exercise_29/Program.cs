a4ba6c853d505a54208343c921dda397aefbbb0b
Amin-BAN <md.amin@centria.fi>
2020-01-25 03:20:47 +0200

    exercise_29 done

--- part1/conditionals/exercise_29/Program.cs
+++ part1/conditionals/exercise_29/Program.cs
@@ -6,6 +6,26 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first number!");
+      string first_input = Console.ReadLine();
+      int firstValue = Convert.ToInt32(first_input);
+
+      Console.WriteLine("Give the second number!");
+      string second_input = Console.ReadLine();
+      int secondValue = Convert.ToInt32(second_input);
+
+      if (firstValue < secondValue)
+      {
+        Console.WriteLine("The larger number is " + secondValue + "!");
+      }
+      else if (firstValue > secondValue)
+      {
+        Console.WriteLine("The larger number is " + firstValue + "!");
+      }
+      else
+      {
+        Console.WriteLine("They are equal!");
+      }
       // Write your code here:
 
     }
