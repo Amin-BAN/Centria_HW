798ad998f138c74ddc5c4afcaa789c4a223bbdcc
Amin-BAN <md.amin@centria.fi>
2020-01-25 02:44:18 +0200

    exercise_28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -6,6 +6,23 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
+       int age = 18;
+
+      Console.WriteLine("How old are you?");
+
+      string input = Console.ReadLine();
+
+      int value = Convert.ToInt32(input);
+
+      if (age < value)
+      {
+        Console.WriteLine("You're an adult!");
+      }
+      else
+      {
+        Console.WriteLine("You're under age!");
+      }
+
       // Write your code here:
 
     }
