10c1a6c1bac415aa28219ec10122924517ab64e4
Amin-BAN <md.amin@centria.fi>
2020-01-28 22:45:32 +0200

    exercise_31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -6,6 +6,18 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a number:");
+      string input = Console.ReadLine();
+      int userinput = Convert.ToInt32(input);
+
+      if (userinput % 2 == 0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
       // Write your code here:
 
     }
