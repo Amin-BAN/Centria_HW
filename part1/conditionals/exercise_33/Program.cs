c8820d899cab649b8053b39de5c0d77b98d2cc72
Amin-BAN <md.amin@centria.fi>
2020-01-28 23:29:49 +0200

    exercise_33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -6,6 +6,20 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first string:");
+      string firstinput = Console.ReadLine();
+
+      Console.WriteLine("Give the second string:");
+      string secondinput = Console.ReadLine();
+
+      if (firstinput == secondinput)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
       // Write your code here:
 
     }
