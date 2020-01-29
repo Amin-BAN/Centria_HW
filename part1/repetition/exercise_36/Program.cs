ca0a8b83591ad64ff10c937783ea07822fa89577
Amin-BAN <md.amin@centria.fi>
2020-01-30 01:24:27 +0200

    Exercise_36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,6 +7,26 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string input = Console.ReadLine();
+        int userinput = Convert.ToInt32(input);
+
+        if (userinput == 0)
+        {
+          break;
+        }
+        else if (userinput < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+        else
+        {
+          Console.WriteLine(userinput * userinput);
+        }
+
+      }      
 
     }
   }
