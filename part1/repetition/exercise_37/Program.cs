74e33be8e3106d234687632d5d7c0e3947803126
Amin-BAN <md.amin@centria.fi>
2020-01-30 18:55:34 +0200

    exercise_37 done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -8,7 +8,24 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string userinput = Console.ReadLine();
+        int input = Convert.ToInt32(userinput);
 
+        if (input == 0)
+        {
+          break;
+        }
+        else
+        {
+          sum = sum + 1;
+        }
+      }
+      {
+        Console.WriteLine("Total amount of numbers: " + sum);
+      }
     }
   }
 }
