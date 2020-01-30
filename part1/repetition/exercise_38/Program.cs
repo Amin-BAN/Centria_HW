27a399f3bb1d610995ce8aa4ea482f60ad6e1ee5
Amin-BAN <md.amin@centria.fi>
2020-01-30 22:03:01 +0200

    exercise_38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,6 +7,28 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
+      int sum = 0;
+      
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string userinput = Console.ReadLine();
+        int input = Convert.ToInt32(userinput);
+
+        if (input == 0)
+        {
+          break;
+        }
+        else if (input < 0)
+        {
+          sum = sum + 1;
+        }
+        else 
+        {
+          continue;
+        }
+      }
+      Console.WriteLine("Total amount of negative numbers: " + sum);
 
     }
   }
