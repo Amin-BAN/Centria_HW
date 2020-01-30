b83460d5bc1b8a5d566e878cf62453b814b7461d
Amin-BAN <md.amin@centria.fi>
2020-01-30 23:32:42 +0200

    exercise_39 done

--- part1/repetition/exercise_39/Program.cs
+++ part1/repetition/exercise_39/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_39
     public static void Main(string[] args)
     {
       // Write your code here:
+      int sum = 0;
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string userinput = Console.ReadLine();
+        int input = Convert.ToInt32(userinput);
+
+        if (input == 0)
+        {
+          break;
+        }
+        else
+        {
+          sum = sum + input;
+        }
+      }
+      {
+        Console.WriteLine("Total sum of numbers: " + sum);
+      }
     }
   }
 }
