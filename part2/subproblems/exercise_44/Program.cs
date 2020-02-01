2cad264e71a3134824b4b8ae13c19007b6921ac6
Amin-BAN <md.amin@centria.fi>
2020-02-01 04:30:08 +0200

    exercise_44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -9,6 +9,22 @@ namespace exercise_44
 
       // Write your code here:
 
+      int input = Convert.ToInt32(Console.ReadLine());
+      int input1 = Convert.ToInt32(Console.ReadLine());
+
+      if (input > input1)
+      {
+        Console.WriteLine(input + " is greater than " + input1 + ".");
+      }
+      else if (input < input1)
+      {
+        Console.WriteLine(input + " is less than " + input1 + ".");
+      }
+      else
+      {
+        Console.WriteLine(input + " is equal to " + input1 +".");
+      }
+
     }
   }
 }
