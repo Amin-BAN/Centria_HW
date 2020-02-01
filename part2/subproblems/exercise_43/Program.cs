f10228bd52a6255554a28ac5657cf103f53ded40
Amin-BAN <md.amin@centria.fi>
2020-02-01 03:12:59 +0200

    exercise_43 done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_43
     public static void Main(string[] args)
     {
       // Write your code here:
+     
+      string userinput = Console.ReadLine();
+      int input = Convert.ToInt32(userinput);
+
+      if (input < 0)
+      {
+        Console.WriteLine(input * -1);
+      }
+      else
+      {
+        Console.WriteLine(input);
+      }
       
     }
   }
