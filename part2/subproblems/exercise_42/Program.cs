a1b167a38dd47c11460bcabd75b69ca947180c70
Amin-BAN <md.amin@centria.fi>
2020-02-01 02:40:06 +0200

    exercise_42 done

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,6 +8,17 @@ namespace exercise_42
     {
 
       // Write your code here:
+
+      string userinput = Console.ReadLine();
+      int input = Convert.ToInt32(userinput);
+
+      string userinput1 = Console.ReadLine();
+      int input1 = Convert.ToInt32(userinput1);
+
+      int sum = input + input1;
+
+      double squareroot = Math.Sqrt(sum);
+      Console.WriteLine(squareroot);
     
     }
   }
