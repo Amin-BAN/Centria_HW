e990394ecae8809d671514ee34f77be3f5a949a4
Amin-BAN <md.amin@centria.fi>
2020-02-02 22:14:22 +0200

    exercise_53 done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -8,10 +8,18 @@ namespace exercise_53
     {
       // Call your method here:
 
-
+      Division(10, 6);
+    
     }
 
     // Write your method here:
+    public static void Division(int numerator, int denominator)
+    {
+      double divAnswer = (double)numerator / denominator;
+      string dotAnswer = Convert.ToString(divAnswer);
+      string converted = dotAnswer.Replace(",", ".").Replace(".", ".");
+      Console.WriteLine(converted);
+    }
 
   }
 }
