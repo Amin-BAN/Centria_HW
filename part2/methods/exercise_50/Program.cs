b04b9927f596feadca3ce32c9434acc733933b22
Amin-BAN <md.amin@centria.fi>
2020-02-02 20:52:59 +0200

    exercise_50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,11 +7,22 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      string userinput = Console.ReadLine();
+      int input = Convert.ToInt32(userinput);
+
+      for (int number = 0; number < input; number = number + 1)
+      {
+        PrintPhrase();
+      }
  
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
+    public static void PrintPhrase() 
+    {
+
+      Console.WriteLine("In a hole in the ground there lived a method");
 
     }
   }
