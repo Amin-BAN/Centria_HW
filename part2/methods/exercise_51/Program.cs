7f0292a44f4d7bae5747d59d7bfa95b575ee63bf
Amin-BAN <md.amin@centria.fi>
2020-02-02 21:20:11 +0200

    exercise_51 done

--- part2/methods/exercise_51/Program.cs
+++ part2/methods/exercise_51/Program.cs
@@ -6,12 +6,23 @@ namespace exercise_51
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
+      // Call your method here:'
 
-
+     PrintUntilNumber(3);
+ 
     }
 
     // Write your method here:
+    public static void PrintUntilNumber(int input)
+    {
+      int number = 1;
+    while (number <= input)
+    {
+      Console.WriteLine(number);
+      number = number + 1;
+    }
+     
+    }
 
   }
 }
