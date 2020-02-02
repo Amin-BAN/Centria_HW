fc03e71f2f9d38d4fb7aa6870874894c837ba952
Amin-BAN <md.amin@centria.fi>
2020-02-02 17:04:07 +0200

    exercise_47 done

--- part2/moreLoops/exercise_47/Program.cs
+++ part2/moreLoops/exercise_47/Program.cs
@@ -8,6 +8,22 @@ namespace exercise_47
     {
 
       // Write your code here:
+
+      Console.WriteLine("Where to?");
+      string userinput = Console.ReadLine();
+      int input = Convert.ToInt32(userinput);
+
+      Console.WriteLine("Where from?");
+      string userinput2 = Console.ReadLine();
+      int input2 = Convert.ToInt32(userinput2);
+
+      int count = input2;
+
+      while (count <= input)
+      {
+        Console.WriteLine(count);
+        count = count + 1;
+      }
       
     }
   }
