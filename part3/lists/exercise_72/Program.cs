14c398b2658676327df8912c485c789b9cff4d4e
Amin-BAN <md.amin@centria.fi>
2020-02-09 04:18:55 +0200

    exercise_72 done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -7,6 +7,35 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> list = new List<int>();
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if ( input == 9999 )
+        {
+          break;
+        } 
+        list.Add(input);
+      }
+      int smallest = list[0];
+
+      for ( int i = 0; i < list.Count; i++ )
+      {
+        if ( list[i] < smallest )
+        {
+          smallest = list[i];
+        }
+      }
+          Console.WriteLine("Smallest number: " + smallest);
+        
+      
+      for ( int i = 0; i < list.Count; i++)
+      { 
+      if ( list[i] == smallest )
+      {
+        Console.WriteLine( "Found at index: " + i );
+      }
+      }
 
     }
   }
