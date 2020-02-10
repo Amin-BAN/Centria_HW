e6f37fc52556ecdd8dd79d7eb75b25ec9ca1e269
Amin-BAN <md.amin@centria.fi>
2020-02-11 00:48:47 +0200

    exercise_85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+     
+
+      Console.WriteLine("Enter username:");
+      string input = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string userinput = Console.ReadLine();
+
+       if ( ( input == "alex" && userinput == "sunshine" ) | ( input == "emma" && userinput == "haskell" ) )
+      {
+        Console.WriteLine( "You have successfully logged in!" );
+      }
+
+      else 
+      {
+        Console.WriteLine( "Incorrect username or password!" );
+      }
 
 
     }
