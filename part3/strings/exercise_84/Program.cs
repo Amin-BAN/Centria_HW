cfd91b642954e2d5758e59424444e3c8900f4580
Amin-BAN <md.amin@centria.fi>
2020-02-10 23:53:04 +0200

    exercise_84 done

--- part3/strings/exercise_84/Program.cs
+++ part3/strings/exercise_84/Program.cs
@@ -7,6 +7,20 @@ namespace exercise_84
   {
     public static void Main(string[] args)
     {
+      string text = "true";
+      string write = "trueish";
+
+      Console.Write("Give a string: ");
+      string input = Console.ReadLine();
+
+      if ( input == text )
+      {
+        Console.WriteLine( "You got it right!" );
+      } 
+      else 
+      {
+        Console.WriteLine( "Try again!" );
+      }
 
 
     }
