6f590bb0676fa49aa150bf32c519ce7eee51ec73
Amin-BAN <md.amin@centria.fi>
2020-02-11 12:17:51 +0200

    exercise_86 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
-
+      while (true)
+      {
+        string input = Console.ReadLine();
+        
+        if (input == "")
+        {
+          break;
+        }
+        string[] pieces = input.Split(" ");
+        foreach( string line in pieces )
+        {
+          Console.WriteLine(line);
+        }
+      }
 
     }
   }
