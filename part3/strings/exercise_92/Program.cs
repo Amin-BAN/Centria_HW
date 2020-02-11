a58fcb027567f77ff99cd41cbff0053ed25d912a
Amin-BAN <md.amin@centria.fi>
2020-02-12 00:00:11 +0200

    exercise_92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,6 +7,31 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      int oldest = Convert.ToInt32(DateTime.Now.Year + 1);
+      string text = ("");
+
+      while (true)
+      {
+        string input = Console.ReadLine();
+
+        if ( input == "" )
+        {
+          break;
+        }
+        string[] pieces = input.Split(",");
+
+        if ( oldest > Convert.ToInt32(pieces[1]) )
+        {
+          oldest = Convert.ToInt32(pieces[1]);
+        }
+        if ( text.Length < pieces[0].Length )
+        {
+          text = pieces[0];
+        }
+      }
+      int old = ( 2020 - oldest );
+      Console.WriteLine("Longest name: " + text);
+      Console.WriteLine("Highest age: " + old);
 
     }
   }
