b9c6c7f8d6e8f76022baae9b93c7e741addba679
Amin-BAN <md.amin@centria.fi>
2020-02-11 12:48:56 +0200

    exercise_87 done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,6 +7,24 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+      while(true)
+      {
+        string input = Console.ReadLine();
+
+        if ( input == "" )
+        {
+          break;
+        }
+        string[] pieces = input.Split(" ");
+        foreach ( string line in pieces )
+        {
+          if ( line.Contains("av") )
+          { 
+            Console.WriteLine(line);
+          }
+        }
+
+      }
 
     }
 
