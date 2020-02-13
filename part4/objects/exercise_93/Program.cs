619b752c9e3dfa1a6d446a45d38c24fbea37bf6a
Amin-BAN <md.amin@centria.fi>
2020-02-13 13:47:31 +0200

    exercise_93 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -9,6 +9,11 @@ namespace exercise_93
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
 
+      Account nabilsAccount = new Account("Nabil's account", 100.00);
+      nabilsAccount.Deposit(20.0);
+      Console.WriteLine(nabilsAccount.balance);
+
+
     }
   }
 }
