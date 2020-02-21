be641c458cf752294665f41e436a3ed493674a06
Amin-BAN <md.amin@centria.fi>
2020-02-21 21:13:58 +0200

    exercise_98 done

---
+++ part4/objects/exercise_98/Product.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace exercise_98
+{
+    public class Product
+    {
+        private double price;
+        private int quantity;
+        private string name;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine( name + ":" + " price " + price + ": " + quantity  + " pcs" );
+
+        }
+    }
+}
