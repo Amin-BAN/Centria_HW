00b1fc13f41ad01bd2cd316795abba60f54392e8
Amin-BAN <md.amin@centria.fi>
2020-03-19 13:50:19 +0200

    exercise_115 done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_115
     public static void Main(string[] args)
     {
 
+      while (true)
+      {
+        string input = Console.ReadLine();
+
+        if ( input == "end" )
+        {
+          break;
+        }
+
+        int number = Convert.ToInt32(input);
+        
+        Console.WriteLine( number * number * number );
+
+        
+      }
+
     }
   }
 }
