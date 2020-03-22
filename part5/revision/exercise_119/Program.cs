3dc6b472cf0be876d8a83fb3343b67588f600a4e
Amin-BAN <md.amin@centria.fi>
2020-03-22 03:25:56 +0200

    exercise_119 done

--- part5/revision/exercise_119/Program.cs
+++ part5/revision/exercise_119/Program.cs
@@ -8,7 +8,26 @@ namespace exercise_119
   {
     public static void Main(string[] args)
     {
-
+       Timer timer = new Timer();
+      
+      while (true)
+      {
+        Console.WriteLine(timer);
+        timer.Advance();
+        
+        try
+        {
+          
+          System.Threading.Thread.Sleep(100);
+        }
+         
+        catch (Exception e)
+        {
+          Console.WriteLine("Error happened: +" + e);
+        }
+      }
     }
   }
 }
+
+    
