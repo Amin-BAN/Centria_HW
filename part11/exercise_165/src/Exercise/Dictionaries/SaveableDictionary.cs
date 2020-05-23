c8058105ba183f040a3f6acb3f3fdea5389a7864
Amin-BAN <md.amin@centria.fi>
2020-05-23 08:23:35 +0300

    fix it

--- part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
+++ part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
@@ -1,39 +1,127 @@
 namespace Exercise
 {
-  using System;
-  using System.IO;
   using System.Collections.Generic;
+  using System.IO;
+  using System;
   public class SaveableDictionary
   {
+    private Dictionary<string, string> dict;
+
+    private string file;
+
     public SaveableDictionary()
     {
+      this.dict = new Dictionary<string, string>();
     }
 
     public SaveableDictionary(string file) : this()
     {
+      this.file = file;
     }
 
     public void Add(string word, string translation)
     {
+      if (!this.dict.ContainsKey(word) && !this.dict.ContainsKey(translation))
+      {
+        this.dict.Add(word, translation);
+        this.dict.Add(translation, word);
+      }
     }
 
     public bool Load()
     {
-      return false;
+      try
+      {
+        string[] lines = File.ReadAllLines(this.file);
+
+        foreach (string line in lines)
+        {
+          string[] parts = line.Split(":");
+
+          // nicer way to do it
+          string word = parts[0];
+          string translation = parts[1];
+          Add(word, translation);
+        }
+        // if (!this.dict.ContainsKey(parts[0]) && !this.dict.ContainsKey(parts[1]))
+        // {
+        //   this.dict.Add(parts[0], parts[1]);
+        //   this.dict.Add(parts[1], parts[0]);
+        return true;
+      }
+
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return false;
+      }
     }
 
     public bool Save()
     {
-      return false;
+      List<string> alreadySaved = new List<string>();
+      try
+      {
+        StreamWriter writer = new StreamWriter(this.file);
+
+        foreach (string word in this.dict.Keys)
+        {
+          string composition = word + ":" + this.dict[word];
+          string backwards = this.dict[word] + ":" + word;
+          if (!alreadySaved.Contains(composition) && !alreadySaved.Contains(backwards))
+          {
+            alreadySaved.Add(composition);
+            writer.WriteLine(composition);  
+          }
+
+          // writer.WriteLine(words.Key + ":" + words.Value);
+        }
+        writer.Close();
+        return true;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+
+      //   foreach (var words in this.dict)
+      //   {
+      //     writer.WriteLine(words.Key + ":" + words.Value);
+      //   }
+      //   writer.Close();
+      //   return true;
+      // }
+      // catch (Exception)
+      // {
+      //   return false;
+      // }
     }
 
     public string Translate(string word)
     {
+      if (this.dict.ContainsKey(word))
+      {
+        return this.dict[word];
+      }
       return null;
     }
 
     public void Delete(string word)
     {
+      // both works
+      // foreach (var words in this.dict)
+      // {
+      //   if (words.Key == word || words.Value == word)
+      //   {
+      //     this.dict.Remove(words.Key);
+      //   }
+      // }
+      if (this.dict.ContainsKey(word))
+      {
+        string translation = this.dict[word];
+        this.dict.Remove(word);
+        this.dict.Remove(translation);
+      }
     }
   }
 }
--- part11/exercise_165/src/Exercise/Program.cs
+++ part11/exercise_165/src/Exercise/Program.cs
@@ -1,12 +1,78 @@
 namespace Exercise
 {
-  using System;
   using System.Collections.Generic;
+  using System;
   public class Program
   {
     public static void Main(string[] args)
     {
 
+      SaveableDictionary dictionary = new SaveableDictionary("words.txt");
+      bool wasSuccessful = dictionary.Load();
+
+      if (wasSuccessful)
+      {
+        Console.WriteLine("Successfully loaded the dictionary from file");
+      }
+
+      // // // // // Translate all the words in the file both ways
+      Console.WriteLine(dictionary.Translate("apina"));
+      Console.WriteLine(dictionary.Translate("monkey"));
+      Console.WriteLine(dictionary.Translate("beer"));
+      Console.WriteLine(dictionary.Translate("olut"));
+      Console.WriteLine(dictionary.Translate("below"));
+      Console.WriteLine(dictionary.Translate("alla oleva"));
+
+      // // // // // Try adding, translating and removing a word, this should not affect the file
+      dictionary.Add("poista", "remove");
+      Console.WriteLine(dictionary.Translate("remove"));
+      dictionary.Delete("remove");
+
+      // // // // // Save the file
+      dictionary.Save();
+
+      // Console.WriteLine(dictionary.Translate("apina"));
+      // Console.WriteLine(dictionary.Translate("ohjelmointi"));
+      // Console.WriteLine(dictionary.Translate("alla oleva"));
+
+      // SaveableDictionary dictionary = new SaveableDictionary();
+      // dictionary.Add("apina", "monkey");
+      // dictionary.Add("banaani", "banana");
+      // dictionary.Add("apina", "apfe");
+      // dictionary.Add("ohjelmointi", "programming");
+
+      // dictionary.Delete("apina");
+
+      // Console.WriteLine(dictionary.Translate("apina"));
+      // Console.WriteLine(dictionary.Translate("monkey"));
+      // Console.WriteLine(dictionary.Translate("banana"));
+      // Console.WriteLine(dictionary.Translate("banaani"));
+      // Console.WriteLine(dictionary.Translate("ohjelmointi"));
+
+      // dictionary.Delete("apina");
+      // dictionary.Delete("banana");
+
+      // SaveableDictionary dictionary = new SaveableDictionary();
+      // dictionary.Add("apina", "monkey");
+      // dictionary.Add("banaani", "banana");
+      // dictionary.Add("apina", "apfe");
+
+      // Console.WriteLine(dictionary.Translate("apina"));
+      // Console.WriteLine(dictionary.Translate("monkey"));
+      // Console.WriteLine(dictionary.Translate("ohjelmointi"));
+      // Console.WriteLine(dictionary.Translate("banana"));
+
+      if (wasSuccessful)
+      {
+        Console.WriteLine("Successfully loaded the dictionary from file");
+      }
+      else
+      {
+        Console.WriteLine("File not loaded!");
+      }
+      Console.WriteLine(dictionary.Translate("apina"));
+      Console.WriteLine(dictionary.Translate("ohjelmointi"));
+      Console.WriteLine(dictionary.Translate("alla oleva"));
     }
   }
 }
